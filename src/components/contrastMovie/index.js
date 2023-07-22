22b2716a78d8026838e3c9342b13406e4a6756c7
Luan <luanitachi@hotmail.com>
2023-07-22 00:34:41 -0300

    add via git

--- src/components/contrastMovie/index.js
+++ src/components/contrastMovie/index.js
@@ -1,9 +1,15 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import { Section } from './styles.js'
+import { Section, ContrastWindowVertical, ContrastWindowHorizontal } from './styles.js'
 
 function Contrast (props) {
+  const firstDate = new Date(props.item.first_air_date)
+  const genres = []
+
+  for (const i in props.item.genres) {
+    genres.push(props.item.genres[i].name)
+  }
   return (
         <Section style={{
           backgroundImage: `url(http://image.tmdb.org/t/p/original${props.item.backdrop_path})`,
@@ -11,7 +17,26 @@ function Contrast (props) {
           backgroundPosition: 'center'
         }}>
 
-            <div>{props.item.name}</div>
+          <ContrastWindowVertical>
+            <ContrastWindowHorizontal>
+            <div className='movie-name'>{props.item.name}</div>
+            <div className='movie-infos'>
+              <div>{props.item.vote_average} pontos</div>
+              <div>{firstDate.getFullYear()}</div>
+              <div>{props.item.number_of_seasons} temporada{props.item.number_of_seasons === 1 ? '' : 's'}</div>
+            </div>
+            <div className='movie-description'>
+                {props.item.overview}
+            </div>
+            <div className='movie-buttons'>
+              <button><a href={`/watch${props.item.id}`}>► Assistir</a></button>
+              <button><a href={`/list/add${props.item.id}`}>+ Minha Lista</a></button>
+            </div>
+            <div className='movie-genres'>Gêneros: {genres.join(', ')}</div>
+
+            </ContrastWindowHorizontal>
+          </ContrastWindowVertical>
+
         </Section>
   )
 }
--- src/components/contrastMovie/styles.js
+++ src/components/contrastMovie/styles.js
@@ -4,3 +4,20 @@ export const Section = styled.section`
     height: 100vh;
     width: 100vw;
 `
+
+export const ContrastWindowVertical = styled.div`
+    width: inherit;
+    height: inherit;
+    background: linear-gradient(to top, #111 10%, transparent 90%);
+`
+
+export const ContrastWindowHorizontal = styled.div`
+    width: inherit;
+    height: inherit;
+    background: linear-gradient(to right, #111 30%, transparent 70%);
+
+    .movie-infos {
+        display: flex;
+        gap: 10px;
+    }
+`
